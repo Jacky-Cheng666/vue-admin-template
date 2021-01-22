bc49d70af047666712aa56a04075cc5900c77804
username@email.com <1260027439@qq.com>
2021-01-22 18:00:30 +0800

    修改logo

--- src/settings.js
+++ src/settings.js
@@ -1,6 +1,6 @@
 module.exports = {
 
-  title: 'Vue Admin Template',
+  title: '财务云',
 
   /**
    * @type {boolean} true | false
@@ -12,6 +12,6 @@ module.exports = {
    * @type {boolean} true | false
    * @description Whether show the logo in sidebar
    */
-  sidebarLogo: false,
+  sidebarLogo: true,
   tagsView: true,
 }
